ac54537ec0c78f3e07def5b8f6888e5bb64492c9
Saeed <DaneshJoy.ir@gmail.com>
2025-04-12 09:23:30 +0330

    updated bmi and lottery codes

--- codes/00 - lottery/lottery.py
+++ codes/00 - lottery/lottery.py
@@ -1,10 +1,21 @@
-phone_number = "09915648345"
+import random
 
-first_4_digits = phone_number[:4]
-last_3_numbers = int(phone_number[-3:])
+# Create a list of numbers
+phone_numbers = []
+pre = "09"
+for i in range(10):
+    numbers = random.randint(100_000_000, 999_999_999)
+    phone_number = f'{pre}{numbers}'
+    phone_numbers.append(phone_number)
+
+winner = random.choice(phone_numbers)
+
+first_4_digits = winner[:4]
+last_3_numbers = int(winner[-3:])
 stars = '***'
 
 # Concatenation
 # winner = first_4_digits + stars + last_3_numbers
 
-print('The winner is: ', first_4_digits, stars, last_3_numbers, sep='')
+print('The winner is: ', first_4_digits,stars, last_3_numbers,
+      sep='')
--- codes/01 - bmi/bmi_calculator.py
+++ codes/01 - bmi/bmi_calculator.py
@@ -1,41 +1,46 @@
-# import sys
-''' Get user's height and weight '''
-''' گرفتن قد و وزن کاربر '''
-weight = input('Please enter your weight (kg): ')
-
-# if not weight.isdigit():
-#     print('wrong input!')
-#     sys.exit(0)
+while True:
+    print('What to do?')
+    print('\t1: Calculate BMI')
+    print('\t2: Exit')
     
-height = input('Please enter your height (cm): ')
-
-weight = weight.strip()
-height = height.strip()
-
-''' Check user's inputs '''
-# Error handling
-if weight.replace('.','',1).isdigit() and height.replace('.','',1).isdigit():
-    ''' process inputs '''
-    weight = float(weight)
-    height = float(height)
+    user_choice = input('Select a number: ')
     
-    height = height / 100
+    if user_choice == '2':
+        break
+        # sys.exit(0)
+    # import sys
+    ''' Get user's height and weight '''
+    ''' گرفتن قد و وزن کاربر '''    
+    weight = input('Please enter your weight (kg): ')   
+    height = input('Please enter your height (cm): ')
     
-    ''' Clculate BMI '''
-    bmi = weight / height**2
-    bmi_rounded = round(bmi, 2)
+    weight = weight.strip()
+    height = height.strip()
     
-    ''' Check user status '''
-    if bmi<18.5:
-        status = 'Underweight'
-    elif bmi>18.5 and bmi<25:
-        status = 'Normal'
+    ''' Check user's inputs '''
+    # Error handling
+    if weight.replace('.','',1).isdigit() and height.replace('.','',1).isdigit():
+        ''' process inputs '''
+        weight = float(weight)
+        height = float(height)
+        
+        height = height / 100
+        
+        ''' Clculate BMI '''
+        bmi = weight / height**2
+        bmi_rounded = round(bmi, 2)
+        
+        ''' Check user status '''
+        if bmi<18.5:
+            status = 'Underweight'
+        elif bmi>18.5 and bmi<25:
+            status = 'Normal'
+        else:
+            status = 'Overweight'
+        
+        ''' Report to user '''
+        print(f'Your BMI is {bmi:.2f} and your status is {status}')
+    
     else:
-        status = 'Overweight'
-    
-    ''' Report to user '''
-    print(f'Your BMI is {bmi:.2f} and your status is {status}')
-
-else:
-    print('Please only enter numbers')
+        print('Please only enter numbers')
 
