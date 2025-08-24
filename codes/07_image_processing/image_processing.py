ed703140b54662429db6b91b529a94f81031303b
Saeed <DaneshJoy.ir@gmail.com>
2025-08-24 15:05:37 +0330

    added projects

--- codes/07_image_processing/image_processing.py
+++ codes/07_image_processing/image_processing.py
@@ -4,11 +4,11 @@ from utils import adjust_image
 from utils import show_images_side_by_side
 from utils import save_results
 
-image_paths = glob('images/*.*g')
+image_paths = glob("images/*.*g")
 
-out_dir = 'results'
+out_dir = "results"
 
-for img_path in image_paths: 
+for img_path in image_paths:
     img_before, img_after = adjust_image(img_path, 1.8, 1.2, 1.2)
     show_images_side_by_side(img_before, img_after)
     save_results(img_after, out_dir, img_path)
